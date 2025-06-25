1ccc89297c09fa914fa293df43965bc1bdb9d53f
Mellon Obada <mellobadah123@gmail.com>
2025-06-25 15:21:37 +0300

    Fix several errors: Hero image, routing, etc.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+
 import Booking from "../pages/Booking";
-import Review from "..pages/Review";
+import Review from "../pages/Review";
 import Hero from '../pages/Hero';
 import Navbar from '../pages/Navbar';
 import Footer from '../pages/Footer';
 import Contact from '../pages/Contact';
-import Review from "../pages/Review";
+
 
 function App() {
   return (    
