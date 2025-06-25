57ee27e0eefe1d5febfc39f2b6c7e2d1f8345367
Elsie <elsieoscar06@gmail.com>
2025-06-25 14:53:49 +0300

    deleted one route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,8 +6,6 @@ import Review from "..pages/Review";
 import Hero from '../pages/Hero';
 import Navbar from '../pages/Navbar';
 import Footer from '../pages/Footer';
-import Contact from '../pages/Contact';
-import Review from "../pages/Review";
 
 function App() {
   return (    
@@ -16,17 +14,11 @@ function App() {
         <Navbar />
         
         <Routes>
-          <Route path="/" element={
-    <>
-    <Hero /> 
-    <Contact/>
-    </>
-    } />
+          <Route path="/" element={<Hero />} />
           <Route path="/booking" element={<Booking />} />
-          <Route path="/review" element={<Review />} />
+          <Route path="/review" element={<Review/>} />
         </Routes>
 
-        
         <Footer />
       </div>
     </Router>
