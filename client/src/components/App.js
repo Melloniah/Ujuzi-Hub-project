dc5c3e3fd61184ab7b29c6250a801a1b3b33a090
Brian <briankagia24@gmail.com>
2025-06-25 05:56:15 +0300

    adds Navbar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
-
+// import React, { useEffect, useState } from "react";
+// import { Switch, Route } from "react-router-dom";
+import React from "react";
 import Hero from '../pages/Hero';
+import Navbar from '../pages/Navbar';
 
 
 function App() {
   return (
   <div className="App">
       <Hero />
+      <Navbar/>
     </div>
   );
 }
