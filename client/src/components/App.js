9911a4cc1f5e98ba31c152b80bf0e1365ea66de1
Elsie <elsieoscar06@gmail.com>
2025-06-25 09:33:06 +0300

    added-a-booking-page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Booking from "../pages/Booking";
 
 import Hero from '../pages/Hero';
 
 function App() {
-  return (
-    <div className="App">
-      <Hero />
-    </div>
+  return (    
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hero/>} />
+        <Route path='/booking' element={<Booking/>} />
+      </Routes>      
+    </Router>
   );
 }
 
---
+++ client/src/pages/Booking.jsx
@@ -0,0 +1,43 @@
+import { useLocation } from 'react-router-dom';
+import {useState, useEffect } from 'react';
+import BookingForm from '../components/BookingForm';
+import Bookingcard from '../components/BookingCard';
+
+function Booking(){
+    const location = useLocation;
+    const { worker } = location.state || {};
+
+    const [bookings, setBookings] = useState([])
+
+    function fetchBookings() {
+        fetch('/bookings')
+        .then((res) => res.json())
+        .then((data) => setBookings(data))
+        .catch(() => alert('Failed to fetch bookings'));
+    }
+    useEffect(() => {
+        fetchBookings();
+    }, [])
+
+    if (!worker) return <p>No fundi selected. Go back and choose a worker.</p>
+
+    return (
+        <div  style={{ maxWidth: '600px', margin: '0 auto' }} >
+            <h2>Book an Appointment with {worker.name}</h2>
+
+            <BookingForm worker={worker} onBook={fetchBookings} />
+
+            <h3 style={{ marginTop: '2rem' }} > All Appointments </h3>
+            {bookings.length === 0 ? (
+                <p> No bookings yet.</p>
+            ): (
+                bookings.map((booking) => (
+                    <Bookingcard key={booking.id} booking={booking} />
+
+                ))
+            )}
+        </div>
+    );
+}
+
+export default Booking
